93aff9bcc1beb5ef4df212a5fafc6ee6dc924b16
Starfrich <cryptobethogle@gmail.com>
2024-09-01 16:29:05 +0700

    index

--- index.sh
+++ index.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
-exists()
-{
-  command -v "$1" >/dev/null 2>&1
+
+check_command() {
+  type "$1" &> /dev/null
 }
-if exists curl; then
-  echo ""
+
+if check_command curl; then
+  echo "curl is already installed."
 else
-  echo "Installing curl..."
-  if ! sudo apt update && sudo apt install curl -y < "/dev/null"; then
-    echo "Failed to install curl. Exiting."
+  echo "curl is not installed. Installing..."
+  if ! sudo apt-get install -y curl < "/dev/null"; then
+    echo "Failed to install curl. Aborting."
     exit 1
   fi
 fi
-bash_profile=$HOME/.bash_profile
-if [ -f "$bash_profile" ]; then
-    source $HOME/.bash_profile
+
+profile_file="$HOME/.bash_profile"
+if [ -e "$profile_file" ]; then
+  . "$profile_file"
 fi
+
 sleep 1
 
-cd $HOME
-sudo install -m 0755 -d /etc/apt/keyrings
+cd "$HOME"
+sudo mkdir -m 0755 -p /etc/apt/keyrings
 sudo rm -f /etc/apt/keyrings/docker.gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo chmod 0644 /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -qy
+sudo apt-get upgrade -qy
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo docker --version
 sudo docker pull nillion/retailtoken-accuser:v1.0.0
 
-mkdir -p $HOME/nillion/accuser
-sudo docker run -v $HOME/nillion/accuser:/var/tmp nillion/retailtoken-accuser:v1.0.0 initialise
+accuser_dir="$HOME/nillion/accuser"
+mkdir -p "$accuser_dir"
+sudo docker run -v "$accuser_dir:/var/tmp" nillion/retailtoken-accuser:v1.0.0 initialise
